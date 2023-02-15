87f7ace10b3c3d3ba635f8a4a75c1cf686405df4
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2023-02-16 00:41:10 +0800

    importing modules to the program

---
+++ project.py
@@ -0,0 +1,9 @@
+print("\n\t\t\t***************  PROGRAMMED BY  **********")
+print("\t\t\t*********** JIMWELL L. MANGULABNAN *******")
+print("\t\t\t**************** BSCOE 2-2 ***************")
+print()
+
+from tkinter import *
+import sqlite3
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
